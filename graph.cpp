50cb13528577da0440de8aa00dc8835629e0582b
Nikita Serba <nikitaserba@icloud.com>
2020-03-07 19:09:41 +0200

    Add copyright header

--- graph.cpp
+++ graph.cpp
@@ -1,3 +1,19 @@
-
+/**
+ * This file is part of cppgraphlib.
+ *
+ *  cppgraphlib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  cppgraphlib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with cppgraphlib.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
 
 #include "graph.h"
